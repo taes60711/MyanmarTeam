ff8760817875392c5877126b4d8cd8c0a99d335e
kitaDEV-stack <kitadev1807@gmail.com>
2025-11-17 11:38:27 +0900

    responsive added

--- js/script.js
+++ js/script.js
@@ -1,3 +1,27 @@
+const menuBar = document.getElementById("menu-bar");
+const navBar = document.querySelector(".navbar");
+
+menuBar.addEventListener("click", (e) =>{
+  e.stopPropagation();
+  navBar.classList.toggle("active");
+})
+
+window.addEventListener("click", (e) =>{
+  if(!e.target.closest("#menu-bar") && !e.target.closest(".navbar")){
+    navBar.classList.remove("active");
+  }
+})
+window.addEventListener("scroll", ()=>{
+  navBar.classList.remove("active");
+})
+
+const navLinks = document.querySelectorAll(".navbar a");
+    navLinks.forEach(link => {
+      link.addEventListener("click", () => {
+        navBar.classList.remove("active");
+      });
+    });
+
 var swiper = new Swiper(".mySwiper", {
   spaceBetween: 30,
   centeredSlides: true,
