1bf6f3c82da59ec77a513252da46f0983e001f21
kitaDEV-stack <kitadev1807@gmail.com>
2025-11-21 11:14:54 +0900

    website finished

--- js/script.js
+++ js/script.js
@@ -21,6 +21,21 @@ const navLinks = document.querySelectorAll(".navbar a");
         navBar.classList.remove("active");
       });
     });
+    // const section = document.querySelectorAll("section");
+    // section.forEach((sec) => {
+    //   let top = window.scrollY;
+    //   let height = sec.offsetHeight;
+    //   let offset = sec.offsetTop - 150;
+    //   let id = sec.getAttribute("id");
+
+    //   if(top >= offset && top < offset + height){
+    //     navLinks.forEach((links) => {
+    //       links.classList.remove("active");
+    //       document.querySelector("header .navbar a[href*=" + id + "]")
+    //       .classList.add("active");
+    //     })
+    //   }
+    // })
 
 var swiper = new Swiper(".mySwiper", {
   spaceBetween: 30,
